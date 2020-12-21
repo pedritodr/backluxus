a3a3013c784d48ecae24258d7cca62cd611ebbcc
Pedro Luis <pedro@datalabcenter.com>
2020-12-21 16:36:11 -0500

    tuberia

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+echo "Deploy script started"
+cd /var/www/pagahoy.com/luxus
+git pull origin master
+echo "Deploy script finished execution"
+exit
+#comentario de prueba para notar el deployment
+#segundo comentario para la prueba
